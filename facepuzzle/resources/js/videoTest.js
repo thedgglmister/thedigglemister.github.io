8350374d4f35565f496ef984aa38c3cbd1032f7d
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-02-24 10:19:41 -0800

    added facepuzzle

--- facepuzzle/resources/js/videoTest.js
+++ facepuzzle/resources/js/videoTest.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
     function main() {
         if (mobile)
-            $("body").append("<p id='mobile_error'>Face Puzzle doesn't currently support mobile browsers</p>")
+            $("body").html("<p id='mobile_error'>Face Puzzle doesn't currently support mobile browsers</p>")
         else {
 
             var emptySlot = 9;
